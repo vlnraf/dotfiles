68cf18ad767b7a78994ccec93e66e3c14f898c1c
vlnraf <raffaele.villani17@gmail.com>
2020-10-15 10:18:16 +0200

    add script to run multiple screen correctly

---
+++ .screenlayout/screen.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --primary --mode 1920x1080 --rate 144.00 --pos 1920x0 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-0 --off --output DVI-1 --off
