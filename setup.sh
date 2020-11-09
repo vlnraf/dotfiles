0aeec109d4d2948f7e03191be69759e2d3790a16
vlnraf <raffaele.villani17@gmail.com>
2020-11-09 22:51:58 +0100

    scprit to restore the dotfiles on other computers

---
+++ setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# https://github.com/vlnraf
+# Author: vlnraf
+
+# Arch aur
+
+install_aur()
+    {
+        cd /tmp/
+        curl -LO https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz
+        tar xvzf yay.tar.gz
+        cd yay
+        makepkg -sci
+    }
+
+# Dotfiles with git bare
+install_dots()
+    {
+        cd ~
+        mkdir ~.config
+        chmod 700 ~/.config
+        git clone -- bare https://github.com/vlnraf/dotfiles $HOME/.dotfiles
+        git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout
+        git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config -- local status.showUntrackedFiles no
+    }
+
+# Disable startx autostart
+temp_auto()
+  {
+    sed -i '70 s/^/#/' ~/.zprofile
+    sed -i '71 s/^/#/' ~/.zprofile
+  }
+
+# Install Xmonad
+install_xmonad()
+    {
+    }
