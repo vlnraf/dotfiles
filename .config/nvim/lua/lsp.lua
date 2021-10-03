7eaa75acfb4613b10f9c1d511684d8afc375c261
vlnraf <raffaele.villani17@gmail.com>
2021-10-03 12:32:32 +0200

    update

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,5 +1,5 @@
 lua << EOF
 -- require'lspconfig'.pyls.setup{on_attach=require'completion'.attach}
--- require'lspconfig'.pyright.setup{on_attach=require'completion'.attach}
+require'lspconfig'.pyright.setup{on_attach=require'completion'.attach}
 require'lspconfig'.clangd.setup{on_attach=require'completion'.attach}
 EOF
